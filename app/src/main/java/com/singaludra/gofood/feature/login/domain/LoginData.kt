1695268a4f583d219f9949648e8fa0b2369ce503
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-09-23 22:31:33 +0700

    feat: create model for login and register and usecases

---
+++ app/src/main/java/com/singaludra/gofood/feature/login/domain/LoginData.kt
@@ -0,0 +1,6 @@
+package com.singaludra.gofood.feature.login.domain
+
+data class LoginData(
+    val email: String,
+    val password: String
+)
---
+++ app/src/main/java/com/singaludra/gofood/feature/register/domain/User.kt
@@ -0,0 +1,19 @@
+package com.singaludra.gofood.feature.register.domain
+
+
+data class RegisterUserItem(
+    val accesToken: String,
+    val tokenType: String,
+    val user: User
+)
+
+data class User(
+    val id: Int = 1,
+    val name: String,
+    val email: String,
+    val address: String,
+    val houseNumber: String,
+    val phoneNumber: String,
+    val city: String,
+    val profile_photo_url: String?
+)
