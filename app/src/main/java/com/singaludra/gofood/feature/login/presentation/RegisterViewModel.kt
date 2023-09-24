e5b77f9eb32e10dcdb41debdc844619cb225a4df
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-09-24 16:42:55 +0700

    feat: create viewModel login, slicing ui login and add navigation

---
+++ app/src/main/java/com/singaludra/gofood/feature/login/presentation/RegisterViewModel.kt
@@ -0,0 +1,33 @@
+package com.singaludra.gofood.feature.login.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.singaludra.gofood.feature.register.domain.RegisterUser
+import com.singaludra.gofood.feature.register.presentation.LoginUIState
+import com.singaludra.gofood.main.factories.register.RemoteRegisterUserFactory
+
+sealed interface RegisterUIState{
+    data class Error(val errorMessage: String?): RegisterUIState
+
+    object Success: RegisterUIState
+    object Loading: RegisterUIState
+}
+
+class RegisterViewModel(
+    private val registerUser: RegisterUser
+): ViewModel() {
+
+    companion object {
+        val FACTORY : ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                RegisterViewModel(
+                    RemoteRegisterUserFactory.createRemoteRegisterUser()
+                )
+            }
+        }
+    }
+
+
+}
