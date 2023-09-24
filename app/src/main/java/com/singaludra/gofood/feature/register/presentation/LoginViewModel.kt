65c7d210b3bf4a93049875b1d9557f4bd53df69a
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-09-24 16:44:27 +0700

    feat: initiate model register and slicing register screen

---
+++ app/src/main/java/com/singaludra/gofood/feature/register/presentation/LoginViewModel.kt
@@ -0,0 +1,65 @@
+package com.singaludra.gofood.feature.register.presentation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.singaludra.gofood.feature.login.domain.LoginData
+import com.singaludra.gofood.feature.login.domain.LoginUser
+import com.singaludra.gofood.feature.login.domain.LoginUserResult
+import com.singaludra.gofood.feature.register.http.usecases.Connectivity
+import com.singaludra.gofood.feature.register.http.usecases.InvalidData
+import com.singaludra.gofood.main.factories.login.LoginUserHttpClientFactory
+import com.singaludra.gofood.main.factories.login.RemoteLoginUserFactory
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+sealed interface LoginUIState{
+    data class Error(val errorMessage: String?): LoginUIState
+
+    object Success: LoginUIState
+    object Loading: LoginUIState
+}
+
+class LoginViewModel(
+    private val loginUser: LoginUser
+): ViewModel() {
+    private val _loginUIState = MutableStateFlow<LoginUIState>(LoginUIState.Loading)
+    val loginUIState = _loginUIState.asStateFlow()
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            val loginData = LoginData(
+                email, password
+            )
+            loginUser.login(loginData).collect{ result ->
+                when (result) {
+                    is LoginUserResult.Success -> {
+                        _loginUIState.value = LoginUIState.Success
+                    }
+                    is LoginUserResult.Failure -> {
+                        val message = when(result.throwable) {
+                            is Connectivity -> "Connectivity"
+                            is InvalidData -> "Invalid Data"
+                            else -> "Something Went Wrong"
+                        }
+
+                        _loginUIState.value = LoginUIState.Error(message)
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        val FACTORY : ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                LoginViewModel(
+                    RemoteLoginUserFactory.createRemoteLoginUser()
+                )
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/singaludra/gofood/feature/register/ui/navigation/Navigation.kt
@@ -0,0 +1,42 @@
+package com.singaludra.gofood.feature.register.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import com.singaludra.gofood.feature.register.presentation.LoginViewModel
+import com.singaludra.gofood.feature.register.ui.LoginScreen
+
+const val LOGIN_GRAPH_ROUTE = "login-graph"
+const val LOGIN = "login"
+
+fun NavGraphBuilder.loginGraph(
+    onBtnRegisterClick: () -> Unit,
+) {
+    navigation(
+        route = LOGIN_GRAPH_ROUTE,
+        startDestination = LOGIN,
+    ) {
+        composable(LOGIN) {
+            LoginUserRoute(viewModel = viewModel(factory = LoginViewModel.FACTORY), onBtnRegisterClick = onBtnRegisterClick)
+        }
+    }
+}
+
+@Composable
+fun LoginUserRoute (
+    viewModel: LoginViewModel,
+    onBtnRegisterClick: () -> Unit,
+) {
+    val loginUIState by viewModel.loginUIState.collectAsStateWithLifecycle()
+
+    LoginScreen(
+        loginUIState = loginUIState,
+        onBtnRegisterClick = onBtnRegisterClick
+    ) { email, password ->
+        viewModel.login(email, password)
+    }
+}
