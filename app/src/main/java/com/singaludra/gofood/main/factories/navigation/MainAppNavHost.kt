28b99bbd160fdad38e1bc33d5f7fb13af3801cfa
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-09-24 16:45:12 +0700

    feat: add MainAppNavHost, add internet permission

---
+++ app/src/main/java/com/singaludra/gofood/main/factories/navigation/MainAppNavHost.kt
@@ -0,0 +1,34 @@
+package com.singaludra.gofood.main.factories.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.singaludra.gofood.feature.login.ui.navigation.REGISTER_GRAPH_ROUTE
+import com.singaludra.gofood.feature.login.ui.navigation.registerGraph
+import com.singaludra.gofood.feature.register.ui.navigation.LOGIN_GRAPH_ROUTE
+import com.singaludra.gofood.feature.register.ui.navigation.loginGraph
+
+@Composable
+fun MainAppNavHost(
+    modifier: Modifier = Modifier,
+    navHostController: NavHostController = rememberNavController(),
+    startDestination: String = LOGIN_GRAPH_ROUTE,
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navHostController,
+        startDestination = startDestination,
+    ) {
+        loginGraph(
+            onBtnRegisterClick = { navHostController.navigate(REGISTER_GRAPH_ROUTE) }
+        )
+        registerGraph(
+            onArrowClick = { navHostController.popBackStack() },
+            onButtonClick = { destination ->
+                navHostController.navigate(destination)
+            },
+        )
+    }
+}
