86115c1cfe81594e5dd40f3e9bb09cec2db8bc01
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-09-27 08:56:56 +0700

    fix: change parameter for `LoginDataRequest` on httpCLient

--- app/src/main/java/com/singaludra/gofood/feature/login/http/LoginUserHttpClient.kt
+++ app/src/main/java/com/singaludra/gofood/feature/login/http/LoginUserHttpClient.kt
@@ -1,6 +1,7 @@
 package com.singaludra.gofood.feature.login.http
 
 import com.singaludra.gofood.feature.login.domain.LoginData
+import com.singaludra.gofood.feature.login.http.model.LoginDataRequest
 import com.singaludra.gofood.feature.register.http.HttpClientRegisterResult
 import com.singaludra.gofood.feature.register.http.response.RemoteRootRegisterUser
 import kotlinx.coroutines.flow.Flow
@@ -11,5 +12,5 @@ sealed class HttpClientLoginResult {
 }
 
 interface LoginUserHttpClient {
-    fun login(userData : LoginData): Flow<HttpClientLoginResult>
+    fun login(userData : LoginDataRequest): Flow<HttpClientLoginResult>
 }
