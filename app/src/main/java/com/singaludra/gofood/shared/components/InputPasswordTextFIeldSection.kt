89b521f01538bf2dd56ebcc1998019e9d5ec11c4
dimasaryamurdiyan <dimasaryamurdiyan123@gmail.com>
2023-09-27 08:55:10 +0700

    fix: fix onValueChange on InputPasswordTextFieldSection password section

--- app/src/main/java/com/singaludra/gofood/shared/components/InputPasswordTextFIeldSection.kt
+++ app/src/main/java/com/singaludra/gofood/shared/components/InputPasswordTextFIeldSection.kt
@@ -28,88 +28,123 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.singaludra.gofood.ui.theme.GofoodTheme
 
 @Composable
 fun InputPasswordTextFieldSection(
     modifier: Modifier = Modifier,
-    title: String = "",
-    placeholder: String = "",
-    showPassword: Boolean = false,
+    value: String = "",
+    title: String = "Label",
+    placeholder: String = "Placeholder",
+    isShowPassword: Boolean = false,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     onValueChange: (String) -> Unit,
 ) {
-    val textValue = remember { mutableStateOf("") }
-    val focusManager = LocalFocusManager.current
-    val isShowPassword = remember { mutableStateOf(showPassword) }
-    val trailingIcon = remember { mutableStateOf(Icons.Filled.Visibility) }
+    val textValue = remember { mutableStateOf(value) }
 
     Column(modifier = modifier) {
-        Text(text = title, fontSize = 12.sp, fontWeight = FontWeight.Bold)
-        BasicTextField(
-            modifier = modifier
-                .fillMaxWidth(),
+        Text(text = title, fontSize = 14.sp, fontWeight = FontWeight.Bold)
+        PasswordBottomOutlineTextField(
+            placeholder = placeholder,
             value = textValue.value,
             onValueChange = {
-                onValueChange.invoke(it)
                 textValue.value = it
+                onValueChange.invoke(it)
             },
-            textStyle = TextStyle(
-                color = Color.Black,
-                fontSize = 16.sp,
-                letterSpacing = if (!isShowPassword.value) 3.sp else TextUnit.Unspecified
-            ),
-            decorationBox = { innerTextField ->
-                Column {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 6.dp, bottom = 6.dp, end = 6.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
+            initialIsShowPassword = isShowPassword,
+            keyboardOptions = keyboardOptions,
+        )
+    }
+}
 
-                        Box {
-                            if (textValue.value.isEmpty()) {
-                                Text(
-                                    text = placeholder,
-                                    color = Color.Gray,
-                                    fontSize = 16.sp
-                                )
-                            }
-                            innerTextField()
-                        }
+@Composable
+fun PasswordBottomOutlineTextField(
+    modifier: Modifier = Modifier,
+    placeholder: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    initialIsShowPassword: Boolean = false,
+    keyboardOptions: KeyboardOptions,
+) {
+    val focusManager = LocalFocusManager.current
+    val isShowPassword = remember { mutableStateOf(initialIsShowPassword) }
+    val trailingIcon = remember { mutableStateOf(Icons.Filled.VisibilityOff) }
 
-                        Spacer(modifier = Modifier.weight(1f))
-                        IconButton(
-                            modifier = Modifier
-                                .size(16.dp),
-                            onClick = {
-                                isShowPassword.value = !isShowPassword.value
-                                trailingIcon.value = if (isShowPassword.value) Icons.Filled.Visibility
-                                else Icons.Filled.VisibilityOff
-                            }) {
-                            Icon(
-                                imageVector = trailingIcon.value,
-                                contentDescription = "Visibility",
+    BasicTextField(
+        modifier = modifier
+            .fillMaxWidth(),
+        value = value,
+        onValueChange = onValueChange,
+        textStyle = TextStyle(
+            color = Color.Black,
+            fontSize = 16.sp,
+            letterSpacing = if (!isShowPassword.value) 3.sp else TextUnit.Unspecified
+        ),
+        decorationBox = { innerTextField ->
+            Column {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 6.dp, bottom = 6.dp, end = 6.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Box {
+                        if (value.isEmpty()) {
+                            Text(
+                                text = placeholder,
+                                color = Color.Gray,
+                                fontSize = 16.sp
                             )
                         }
+                        innerTextField()
                     }
-                    Divider(color = Color.Gray, thickness = 1.dp)
+
+                    Spacer(modifier = Modifier.weight(1f))
+                    IconButton(
+                        modifier = Modifier
+                            .size(16.dp),
+                        onClick = {
+                            isShowPassword.value = !isShowPassword.value
+                            trailingIcon.value = if (isShowPassword.value) Icons.Filled.Visibility
+                            else Icons.Filled.VisibilityOff
+                        }) {
+                        Icon(
+                            imageVector = trailingIcon.value,
+                            contentDescription = "Visibility",
+                        )
+                    }
                 }
-            },
-            visualTransformation = if (isShowPassword.value) VisualTransformation.None else {
-                PasswordVisualTransformation()
-            },
-            singleLine = true,
-            maxLines = 1,
-            keyboardActions = KeyboardActions(
-                onDone = {
-                    focusManager.clearFocus()
-                }
-            ),
-            keyboardOptions = keyboardOptions,
+                Divider(color = Color.Gray, thickness = 1.dp)
+            }
+        },
+        visualTransformation = if (isShowPassword.value) VisualTransformation.None else {
+            PasswordVisualTransformation()
+        },
+        singleLine = true,
+        maxLines = 1,
+        keyboardActions = KeyboardActions(
+            onDone = {
+                focusManager.clearFocus()
+            }
+        ),
+        keyboardOptions = keyboardOptions,
+    )
+}
+
+
+@Composable
+@Preview(showBackground = true)
+private fun PasswordInputComponentPreview() {
+    GofoodTheme {
+        InputPasswordTextFieldSection(
+            title = "Kata Sandi",
+            placeholder = "Berupa huruf / angka",
+            onValueChange = {}
         )
     }
 }
